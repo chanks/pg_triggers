88bea92759a3f5ac85cead38efd8d25cc8dc4f74
Chris Hanks <christopher.m.hanks@gmail.com>
2014-08-15 14:32:56 -0400

    For counter_cache, only process UPDATEs if any of the related columns changes.

--- lib/pg_triggers.rb
+++ lib/pg_triggers.rb
@@ -3,7 +3,9 @@ require "pg_triggers/version"
 module PgTriggers
   class << self
     def counter_cache(main_table, counter_column, counted_table, relationship, options = {})
-      where = proc { |source| relationship.map{|k, v| "#{k} = #{source}.#{v}"}.join(' AND ') }
+      where   = proc { |source| relationship.map{|k, v| "#{k} = #{source}.#{v}"}.join(' AND ') }
+      columns = relationship.values
+      changed = columns.map{|c| "NEW.#{c} <> OLD.#{c}"}.join(' OR ')
 
       <<-SQL
         CREATE FUNCTION pg_triggers_counter_#{main_table}_#{counter_column}() RETURNS trigger
@@ -14,8 +16,10 @@ module PgTriggers
                 UPDATE #{main_table} SET #{counter_column} = #{counter_column} + 1 WHERE #{where['NEW']};
                 RETURN NEW;
               ELSIF (TG_OP = 'UPDATE') THEN
-                UPDATE #{main_table} SET #{counter_column} = #{counter_column} - 1 WHERE #{where['OLD']};
-                UPDATE #{main_table} SET #{counter_column} = #{counter_column} + 1 WHERE #{where['NEW']};
+                IF (#{changed}) THEN
+                  UPDATE #{main_table} SET #{counter_column} = #{counter_column} - 1 WHERE #{where['OLD']};
+                  UPDATE #{main_table} SET #{counter_column} = #{counter_column} + 1 WHERE #{where['NEW']};
+                END IF;
                 RETURN NEW;
               ELSIF (TG_OP = 'DELETE') THEN
                 UPDATE #{main_table} SET #{counter_column} = #{counter_column} - 1 WHERE #{where['OLD']};
