bd7eb36df62901d1049ed9a7e343e53f69a50f4c
Chris Hanks <christopher.m.hanks@gmail.com>
2018-03-09 15:05:34 -0500

    More spec work.

--- spec/conditional_foreign_key_spec.rb
+++ spec/conditional_foreign_key_spec.rb
@@ -6,11 +6,12 @@ describe PgTriggers, 'conditional_foreign_key' do
     DB.drop_table?(:children)
 
     DB.create_table :parents do
-      integer :id1, null: false
-      integer :id2, null: false
-      primary_key [:id1, :id2]
+      primary_key :id1
+      integer     :id2
 
-      integer :number
+      integer :other_column
+
+      unique [:id1, :id2]
     end
 
     DB.create_table :children do
@@ -18,6 +19,8 @@ describe PgTriggers, 'conditional_foreign_key' do
 
       integer :parent_id1
       integer :parent_id2
+
+      integer :other_column
     end
   end
 
@@ -29,82 +32,86 @@ describe PgTriggers, 'conditional_foreign_key' do
           child_table: :children,
           relationship: {id1: :parent_id1, id2: :parent_id2},
         )
+
+      DB[:parents].insert(id1: 1, id2: 3)
+      DB[:parents].insert(id1: 2, id2: 4)
+      DB[:children].insert(parent_id1: 1, parent_id2: 3)
     end
 
     describe "when modifying a parent" do
-      describe "with an insert" do
-        it "should not throw an error" do
-          DB[:parents].insert(id1: 1, id2: 2)
-        end
-      end
-
       describe "with an update" do
-        before do
-          DB[:parents].insert(id1: 1, id2: 2)
+        it "should not throw an error when updating a parent that has no referring children" do
+          DB[:parents].where(id1: 2).update(id1: 3).should == 1
         end
 
-        it "should not throw an error when updating a parent that has no referring children" do
-          DB[:parents].where(id1: 1, id2: 2).update(id1: 4).should == 1
+        it "should not throw an error when updating a parent that has only a partial key" do
+          DB[:parents].where(id1: 2).update(id2: nil).should == 1
+          DB[:parents].where(id1: 2).update(other_column: 17).should == 1
+          DB[:parents].where(id1: 2).update(id2: 2).should == 1
+        end
+
+        it "should not throw an error when updating non-key columns of a parent that a child is referring to" do
+          DB[:parents].where(id1: 1, id2: 3).update(other_column: 7).should == 1
         end
 
         it "should throw an error when updating the key of a parent that a child is pointing to" do
-          DB[:children].insert(parent_id1: 1, parent_id2: 2)
-
           key = [:id1, :id2].sample
-
           # TODO: Figure out what to raise to make these ForeignKeyConstraint violations.
-          proc{DB[:parents].where(id1: 1, id2: 2).update(key => 3)}.should raise_error(Sequel::DatabaseError, /update in parents violates foreign key constraint/)
+          proc{DB[:parents].where(id1: 1).update(key => 7)}.should raise_error(Sequel::DatabaseError, /update in parents violates foreign key constraint/)
+        end
+
+        it "should throw an error when setting null the key of a parent that a child is pointing to" do
+          proc{DB[:parents].where(id1: 1).update(id2: nil)}.should raise_error(Sequel::DatabaseError, /update in parents violates foreign key constraint/)
         end
       end
 
       describe "with an delete" do
-        before do
-          DB[:parents].insert(id1: 1, id2: 2)
+        it "should not throw an error when deleting a parent that has no referring children" do
+          DB[:parents].where(id1: 2).delete.should == 1
         end
 
-        it "should not throw an error when deleting a parent that has no referring children" do
-          DB[:parents].where(id1: 1, id2: 2).delete.should == 1
+        it "should not throw an error when deleting a parent that has an incomplete key" do
+          DB[:parents].where(id1: 2).update(id2: nil).should == 1
+          DB[:parents].where(id1: 2).delete.should == 1
         end
 
         it "should throw an error when deleting a parent that a child is pointing to" do
-          DB[:children].insert(parent_id1: 1, parent_id2: 2)
-
-          proc{DB[:parents].where(id1: 1, id2: 2).delete}.should raise_error(Sequel::DatabaseError, /delete in parents violates foreign key constraint/)
+          proc{DB[:parents].where(id1: 1).delete}.should raise_error(Sequel::DatabaseError, /delete in parents violates foreign key constraint/)
         end
       end
     end
 
     describe "when modifying a child" do
-      it "should not throw an error when inserting/updating a child whose specified parent exists" do
-        DB[:parents].insert(id1: 1, id2: 2)
-        DB[:parents].insert(id1: 1, id2: 3)
+      describe "with an insert" do
+        it "should not throw an error when the specified parent exists"
 
-        DB[:children].insert(id: 1, parent_id1: 1, parent_id2: 2)
-        DB[:children].where(id: 1).update(parent_id2: 3)
+        it "should throw an error when the specified parent doesn't exist" do
+          proc{DB[:children].insert(id: 2, parent_id1: 1, parent_id2: 4)}.should raise_error(Sequel::DatabaseError, /insert in children violates foreign key constraint/)
+        end
+
+        it "should not throw an error when the child doesn't have a complete foreign key" do
+          skip "Not yet implemented"
+
+          DB[:children].insert(id: 1, parent_id1: 1)
+          DB[:children].where(id: 1).update(parent_id1: 3)
+        end
       end
 
-      it "should throw an error when inserting a child whose specified parent doesn't exist" do
-        DB[:parents].insert(id1: 1, id2: 2)
-        DB[:parents].insert(id1: 1, id2: 3)
+      describe "with an update" do
+        it "should not throw an error when updating a child whose specified parent exists" do
+          DB[:children].where(id: 1).update(other_column: 3).should == 1
+        end
 
-        proc{DB[:children].insert(id: 1, parent_id1: 1, parent_id2: 4)}.should raise_error(Sequel::DatabaseError, /insert in children violates foreign key constraint/)
-      end
+        it "should throw an error when updating a child's foreign key when the new parent doesn't exist" do
+          proc{DB[:children].where(id: 1).update(parent_id2: 4)}.should raise_error(Sequel::DatabaseError, /update in children violates foreign key constraint/)
+        end
 
-      it "should throw an error when updating a child's foreign key when the new parent doesn't exist" do
-        DB[:parents].insert(id1: 1, id2: 2)
+        it "should not throw an error when the new child record doesn't have a complete foreign key" do
+          skip "Not yet implemented"
 
-        DB[:children].insert(id: 1, parent_id1: 1, parent_id2: 2)
-        proc{DB[:children].where(id: 1).update(parent_id2: 3)}.should raise_error(Sequel::DatabaseError, /update in children violates foreign key constraint/)
-      end
-
-      it "should not throw an error when inserting/updating a child with an incomplete foreign key" do
-        DB[:parents].insert(id1: 1, id2: 2)
-        DB[:parents].insert(id1: 1, id2: 3)
-
-        skip "Not yet implemented"
-
-        DB[:children].insert(id: 1, parent_id1: 1)
-        DB[:children].where(id: 1).update(parent_id1: 3)
+          DB[:children].insert(id: 1, parent_id1: 1)
+          DB[:children].where(id: 1).update(parent_id1: 3)
+        end
       end
     end
   end
